fba6615fd2c04c100bb43a689ce9859f79103767
Slawek12Q <slawek.guzik@interia.pl>
2019-02-20 15:27:00 +0100

    add enter to on website

--- Selenium_project/run.py
+++ Selenium_project/run.py
@@ -1,7 +1,7 @@
 from selenium import webdriver
 
-login = input("Please fill in your email")
-password = input("Please fill in your password")
+login = input("Please fill in your email\n")
+password = input("Please fill in your password\n")
 
 browser = webdriver.Chrome(executable_path='C:\driver\chromedriver.exe')
 browser.get('https://www.usosweb.uj.edu.pl/kontroler.php?_action=actionx:news/default()')
@@ -9,4 +9,12 @@ browser.maximize_window()
 
 loggin_button = browser.find_element_by_css_selector('div#casmenu > table > tbody > tr > td:nth-of-type(2) > a')
 loggin_button.click()
+
+loginField = browser.find_element_by_css_selector("input#username")
+passwordField = browser.find_element_by_css_selector("input#password")
+
+loginField.send_keys(login)
+passwordField.send_keys(password)
+passwordField.submit()
+
 
